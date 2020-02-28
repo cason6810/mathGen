a2afbd48e13d1d2cc9d338f86f102204e38cc474
mingminghe <mingminghe@ebnew.com>
2020-02-28 10:28:43 +0800

    第一版，加法和减法

---
+++ src/views/num.vue
@@ -0,0 +1,19 @@
+<template>
+    <span>{{val}}</span>
+</template>
+<script>
+export default {
+    name: "num",
+    props: {
+        data: {
+            type: Object
+        },
+        index: {
+            type: Number
+        },
+        val: {
+            type: [Number, String]
+        }
+    }
+}
+</script>
---
+++ src/views/numInput.vue
@@ -0,0 +1,32 @@
+<template>
+    <span ><input @keyup="test($el)" v-model="inputNum"/></span>
+</template>
+<script>
+export default {
+    name: "numInput",
+    props: {
+        data: {
+            type: Object
+        },
+        index: {
+            type: Number
+        },
+        val: {
+            type: [Number, String]
+        }
+    },
+    data() {
+        return {
+            inputNum: ''
+        }
+    },
+    methods: {
+        test() {
+            this.$emit('subInput',{
+                index: this.index,
+                num: this.inputNum
+            })
+        }
+    }
+}
+</script>
