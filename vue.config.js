e072f78e41ed5a9fd38ba4d83ce2cc83e772733d
mingminghe <mingminghe@ebnew.com>
2020-02-28 11:16:09 +0800

    modify

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,9 @@ module.exports = {
     devServer: {
         port: 9000,
         open: true
-    }
+    },
+    outputDir: 'docs',
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/mathGen/'
+    : '/'
 }
